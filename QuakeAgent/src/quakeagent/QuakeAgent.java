b7a39014d1415599723faf7944fadb0b9cdc3430
neochuky <neochuki@gmail.com>
2013-03-05 13:12:47 +0000

    Primer commit, proyecto creado

---
+++ QuakeAgent/src/quakeagent/QuakeAgent.java
@@ -0,0 +1,12 @@
+package quakeagent;
+
+import jess.*;
+
+public class QuakeAgent {
+
+    public static void main(String[] args) {
+        
+        System.out.printf("Hello world\n");
+        
+    }
+}
