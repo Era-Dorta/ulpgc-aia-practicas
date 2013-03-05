96e63a129a0e51ccd9e733e9edffec087106dc99
neochuky <neochuki@gmail.com>
2013-03-05 13:19:28 +0000

    Añadido el qaselib.jar

--- QuakeAgent/src/quakeagent/QuakeAgent.java
+++ QuakeAgent/src/quakeagent/QuakeAgent.java
@@ -1,5 +1,19 @@
 package quakeagent;
 
+import java.util.Vector;
+import java.util.Random;
+
+import soc.qase.bot.ObserverBot;
+import soc.qase.file.bsp.BSPParser;
+import soc.qase.state.Player;
+import soc.qase.state.PlayerMove;
+import soc.qase.state.World;
+import soc.qase.tools.vecmath.Vector3f;
+
+import soc.qase.state.*;
+
+import java.lang.Math;
+
 import jess.*;
 
 public class QuakeAgent {
