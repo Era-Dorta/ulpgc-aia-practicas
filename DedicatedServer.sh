e33053ca9a9c5f544c4e97fc56300f55bc839f66
neochuky <neochuki@gmail.com>
2013-03-11 20:23:59 +0000

    Corregido el dedicatedServer.sh para usar la Ip del equipo automaticamente

--- DedicatedServer.sh
+++ DedicatedServer.sh
@@ -1,1 +1,3 @@
-yamagiq2 +set dedicated 1 +set deathmatch 1 +set maxclients 32 +map q2dm1 +set in_grab 0  +set ip 192.168.1.61
+#!/bin/bash
+ipAddress=$(ifconfig eth0 | awk '/Direc. inet/ {split ($2,A,":"); print A[2]}')
+yamagiq2 +set dedicated 1 +set deathmatch 1 +set maxclients 32 +map q2dm1 +set in_grab 0  +set ip "$ipAddress"
