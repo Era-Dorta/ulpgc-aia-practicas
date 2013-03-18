34cf3a77dc5772f29ba350f1277fe7c9cca52222
neochuky <neochuki@gmail.com>
2013-03-18 16:07:46 +0000

    El script espectador puedes sacar el raton de la pantalla y del interactivo no

--- ConectSpectatorClient.sh
+++ ConectSpectatorClient.sh
@@ -4,4 +4,4 @@ len=${#ipAddress}
 if [ "$len" = 0 ] ; then
 	ipAddress=$(ifconfig wlan0 | awk '/Direc. inet/ {split ($2,A,":"); print A[2]}')
 fi
-yamagiq2 +connect "[$ipAddress""]:27910" +set spectator 1 +set in_grab 1
+yamagiq2 +connect "[$ipAddress""]:27910" +set spectator 1 +set in_grab 0
