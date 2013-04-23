ebe55eeac3b492940cb2b124fe1c17e7964c8cc1
neochuky <neochuki@gmail.com>
2013-04-23 10:06:02 +0100

    Modificados scripts para que se les pueda pasar una IP como argumento

--- scripts/ConectSpectatorClient.sh
+++ scripts/ConectSpectatorClient.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-ipAddress=$(ifconfig eth0 | awk '/Direc. inet/ {split ($2,A,":"); print A[2]}')
-len=${#ipAddress}
-if [ "$len" = 0 ] ; then
-	ipAddress=$(ifconfig wlan0 | awk '/Direc. inet/ {split ($2,A,":"); print A[2]}')
+if [ "$#" -eq 1 ]
+then
+	ipAddress="$1"	
+else
+	ipAddress=$(ifconfig eth0 | awk '/Direc. inet/ {split ($2,A,":"); print A[2]}')
+	len=${#ipAddress}
+	if [ "$len" = 0 ] ; then
+		ipAddress=$(ifconfig wlan0 | awk '/Direc. inet/ {split ($2,A,":"); print A[2]}')
+	fi  
 fi
+
 yamagiq2 +connect "[$ipAddress""]:27910" +set spectator 1 +set in_grab 0
